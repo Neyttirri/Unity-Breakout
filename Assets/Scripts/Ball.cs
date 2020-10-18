7c92e81d1188aebd7d22c73a0b82e29e88ab7c00
ElTabakova <elena.tabakova@nexnet.de>
2020-10-18 17:41:25 +0200

    test

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	float _speed = 20f;
+	Rigidbody _rigidbody;
+	Vector3 _velocity;	// the actual movement speed of the ball + directions
+	Renderer _renderer;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _renderer = GetComponent<Renderer>();
+        Invoke("Launch", 0.5f); 	// delay half a second before moving the new ball (instantiate it and waits before moving)  
+    }
+
+	void Launch()
+	{
+		_rigidbody.velocity = Vector3.up * _speed;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    // runs by def 100 times a sec
+    void FixedUpdate()
+    {
+    	_rigidbody.velocity = _rigidbody.velocity.normalized * _speed;
+    	_velocity = _rigidbody.velocity;
+    	
+    	if (!_renderer.isVisible)
+    	{
+    		GameManager.Instance.Balls--;
+    		Destroy(gameObject);
+    	}
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+    	_rigidbody.velocity =  Vector3.Reflect(_velocity, collision.contacts[0].normal);
+    }
+}
---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int hits = 1;
+    public int points = 100; 
+    public Vector3 rotator;
+    public Material hitMaterial;
+    
+    private Material _origMaterial;
+    private Renderer _renderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(rotator * (transform.position.x + transform.position.y) * 0.1f);
+        _renderer = GetComponent<Renderer>();
+        _origMaterial = _renderer.sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotator * Time.deltaTime);
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+    	hits--;
+    	// score points
+    	if (hits <= 0)
+    	{
+    		GameManager.Instance.Score += points; 
+    		Destroy(gameObject);	// gameObject is a predefined variable that access the object this script is added to 
+    	}
+    	_renderer.sharedMaterial = hitMaterial;
+    	Invoke("RestoreMaterial", 0.05f);	// invoke the method after that many sec 
+    }
+    
+    void RestoreMaterial()
+    {
+    	_renderer.sharedMaterial = _origMaterial;
+    }
+}
